bf204b166413858d0c3e1dc8a5be897c841c10ba
masamichhhhi <masamichhhi@gmail.com>
2022-02-14 11:17:05 +0900

    add rdb impl

--- storage_rdb_impl.go
+++ storage_rdb_impl.go
@@ -19,10 +19,64 @@ func NewDBClient(dbConfig *DBConfig) (*sqlx.DB, error) {
 	return db, nil
 }
 
+type StorageRdbImpl struct {
+	DB *sqlx.DB
+}
+
+func NewStorageRdbImpl(db *sqlx.DB) StorageRdbImpl {
+	return StorageRdbImpl{
+		DB: db,
+	}
+}
+
 type DBConfig struct {
 	User     string
 	Password string
 	Addr     string
 	Port     string
 	DB       string
+}
+
+func (s StorageRdbImpl) AddDocument(doc Document) (DocumentID, error) {
+	res, err := s.DB.NamedExec(`insert into documents (body, token_count) values (:body, :token_count)`,
+		map[string]interface{}{
+			"body":        doc.Body,
+			"token_count": doc.TokenCount,
+		})
+
+	if err != nil {
+		return 0, err
+	}
+
+	insertedID, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return DocumentID(insertedID), err
+}
+
+func (s StorageRdbImpl) UpsertInvertedIndex(inverted InvertedIndex) error {
+	encoded, err := encode(inverted)
+	if err != nil {
+		return err
+	}
+
+	for _, v := range encoded {
+		_, err := s.DB.NamedExec(
+			`insert into inverted_indexes (token_id, posting_list)
+			values (:token_id, :posting_list)
+			on duplicate key update posting_list = :posting_list`, v)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func encode(invertedIndex InvertedIndex) ([]EncodedInvertedIndex, error)
+
+type EncodedInvertedIndex struct {
+	TokenID     TokenID `db:"token_id"`
+	PostingList []byte  `db:"posting_list"`
 }
