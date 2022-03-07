d36aed59cde7dd2999252348c5ad66225a6022b9
masamichhhhi <masamichhhi@gmail.com>
2022-03-07 20:05:37 +0900

    add NewAnalyzer

--- analyzer.go
+++ analyzer.go
@@ -6,6 +6,14 @@ type Analyzer struct {
 	tokenFilters []TokenFilter
 }
 
+func NewAnalyzer(charFilters []CharFilter, tokenizer Tokenizer, tokenFilters []TokenFilter) Analyzer {
+	return Analyzer{
+		charFilters:  charFilters,
+		tokenizer:    tokenizer,
+		tokenFilters: tokenFilters,
+	}
+}
+
 func (a Analyzer) Analyze(s string) TokenStream {
 	for _, c := range a.charFilters {
 		s = c.Filter(s)
